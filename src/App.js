38391f070f17e370bb70bb4a57385817db16032b
Dante <dacerron@gmail.com>
2021-10-05 16:33:33 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
+import { useSpring, a } from '@react-spring/three'
+import { useState, useRef } from 'react'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { useControls } from 'leva'
+import * as THREE from "three"
+
+extend({ OrbitControls })
+
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+  useFrame((delta, state) => {
+    orbitRef.current.update();
+  })
+  return (
+    <orbitControls
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 3}
+      ref={orbitRef}
+      args={[camera, gl.domElement]}
+    />
+  )
+}
+
+const Box = () => {
+  const meshRef = useRef();
+  const [hover, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  const { xPosition, yPosition, zPosition, boxColor, rotationSpeed } = useControls({ xPosition: 0, yPosition: 0, zPosition: 0, boxColor: "#fff", rotationSpeed:0 })
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+  })
+  useFrame(() => {
+    meshRef.current.rotation.y += rotationSpeed
+  })
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+      position={[xPosition, yPosition, zPosition]}
+      castShadow>
+
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]}></boxBufferGeometry>
+      <a.meshPhysicalMaterial attach="material" color={boxColor}></a.meshPhysicalMaterial>
+    </a.mesh>
+  )
+}
+
+const Plane = () => {
+  return (
+    <mesh
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -0.5, 0]}
+      receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[100, 100]}></planeBufferGeometry>
+      <meshPhysicalMaterial attach="material" color={"white"}></meshPhysicalMaterial>
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 5] }} onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true
+      gl.shadowMap.type = THREE.PCFSoftShadowMap
+    }}>
+      <fog attach="fog" args={["white", 5, 15]} />
+      <Controls />
+      <Box></Box>
+      <Plane />
+
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} castShadow />
+    </Canvas>
   );
 }
 
