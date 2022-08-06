24d0330b51b81ca25f30ae122d6ee698db14b2fe
dacerron <dacerron@gmail.com>
2022-08-06 11:33:31 -0700

    merging one last file

--- src/Components/AnimatedSpheres.js
+++ src/Components/AnimatedSpheres.js
@@ -12,17 +12,10 @@ for (let i = 0; i < Math.floor(numSpheres); i++) {
     spheres.push({ color: color, position: [-1.5, 0.5, i * 4], key: i, start: false })
 }
 const AnimatedSphere = animated(Sphere)
-<<<<<<< HEAD
-export const AnimatedSpheres = ({rise}) => {
-    const { sphereShouldRise } = useSpring( {sphereShouldRise: rise? spheres.length : -1, config: {duration: 1000} })
-
-    return (<>
-=======
 export const AnimatedSpheres = ({ menuFunctions }) => {
     const riseValues = React.useContext(SphereContext);
     console.log(riseValues);
     return (menuFunctions?<>
->>>>>>> 99ddd6e66166b29f635db83448096a2ced2b4e25
         {spheres.map((sphere, index) => {
             return index < menuFunctions.length? <AnimatedSphere color={sphere.color} key={sphere.key} position={sphere.position} index={index} menuFunction={menuFunctions[index]} shouldRise={riseValues.values[index]}></AnimatedSphere> : null
         })}
